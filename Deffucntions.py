5add0b6710179abdd03f3dab87a42fe75a8f06a8
Prakhar Bhatnagar <soundsasth@gmail.com>
2026-07-14 12:51:47 +0530

    Version 4 (asking fuel type from owner of car)

--- Deffucntions.py
+++ Deffucntions.py
@@ -3,4 +3,5 @@ def describe_car(brand="Tesla", model="Model S"):
 describe_car()
 brand = input("What is the brand of your car? : ")
 model = input("What is the model of your car? : ")
+fuel_type = input("What is the fuel type of your car? : ")
 describe_car(brand, model)
